4ffa25e4617c8c1bd6f5d97b36783bf9e6f225b2
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-02-10 16:43:59 +0000

    Started implementing cli player decide function not finished need to change argument to be a poitner

--- player/LocalCLIPlayer.cpp
+++ player/LocalCLIPlayer.cpp
@@ -23,12 +23,41 @@ Action LocalCLIPlayer::play() {
     }
 }
 
-Player *LocalCLIPlayer::actionOnPlayer(std::vector<Player *> vector) {
+Player *LocalCLIPlayer::actionOnPlayer(std::vector<Player *> players) {
     return nullptr;
 }
 
-void LocalCLIPlayer::decideCards(std::vector<Card *> vector) {
+void LocalCLIPlayer::decideCards(std::vector<Card *> cards) {
+    std::cout << "Your cards are: ";
+    int i = 1;
+    for (const auto &card: cards)
+        std::cout << std::to_string(i++) << ": " << card->toString() << std::endl;
 
+    while (this->shield->empty()) {
+        std::string input;
+        std::cout << "\nEnter the number(s) of the card(s) for your shield: ";
+        std::cin >> input;
+        size_t delimPos = input.find(',');
+
+        int card1, card2 = -1;
+        try {
+            if (delimPos == std::string::npos) card1 = std::stoi(input);
+            else {
+                card1 = std::stoi(input.substr(delimPos));
+                card2 = std::stoi(input.substr(delimPos, input.length() - 1));
+            }
+        } catch (std::exception &e) {
+            std::cout << "Make sure you enter two integers, seperated by ','\n";
+        }
+
+        if (card1 > 3 || card1 < 1) continue;
+        this->shield->push_back(cards.at(card1));
+        if (card2 > 3 || card2 < 1) continue;
+        this->shield->push_back(cards.at(card2));
+    }
+
+    //todo add other cards to health
+    // todo change argument to pointer
 }
 
 void LocalCLIPlayer::printCards() {
--- player/LocalCLIPlayer.h
+++ player/LocalCLIPlayer.h
@@ -11,9 +11,9 @@ class LocalCLIPlayer : public Player {
 public:
     Action play() override;
 
-    Player *actionOnPlayer(std::vector<Player *> vector) override;
+    Player *actionOnPlayer(std::vector<Player *> players) override;
 
-    void decideCards(std::vector<Card *> vector) override;
+    void decideCards(std::vector<Card *> cards) override;
 
 private:
     void printCards();
