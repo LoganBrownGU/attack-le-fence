e1c45a352dfa8300f5f9ce804bf852f32031e156
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-02-11 12:30:34 +0000

    Added shuffleDeck() function Also implemented a printDeck() function

--- CardFactory.cpp
+++ CardFactory.cpp
@@ -5,6 +5,8 @@
 #include "CardFactory.h"
 
 #include <memory>
+#include <random>
+#include <iostream>
 
 std::vector<std::unique_ptr<Card>> *CardFactory::generateNormalDeck() {
     auto *cards = new std::vector<std::unique_ptr<Card>>;
@@ -15,4 +17,22 @@ std::vector<std::unique_ptr<Card>> *CardFactory::generateNormalDeck() {
     }
 
     return cards;
+}
+
+void CardFactory::shuffleDeck(std::vector<Card *> *deck) {
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_int_distribution<int> distribution(0, (int) deck->size());
+
+    for (int i = 0; i < deck->size(); i++) {
+        int idx = distribution(generator);
+        auto card = deck->at(idx);
+        deck->erase(deck->begin() + idx);
+        deck->push_back(card);
+    }
+}
+
+void CardFactory::printDeck(std::vector<Card *> *deck) {
+    for (const auto &card: *deck)
+        std::cout << card->toString() << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,5 @@
 int main() {
     auto cards = CardFactory::generateNormalDeck();
 
-    for (const auto &card: *cards) {
-        std::cout << card->toString() << std::endl;
-    }
+    CardFactory::printDeck(cards);
 }
