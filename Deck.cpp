18f50ff694d17352c3a22f5b47c0ceceb96654b7
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-02-17 16:47:42 +0000

    implemented deal()

--- Deck.cpp
+++ Deck.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "Deck.h"
+
+std::unique_ptr<std::vector<Card *>> Deck::deal(int n) {
+    auto dealtCards = std::vector<Card *>();
+    for (int i = 0; i < n; i++)
+        dealtCards.push_back(this->bottom());
+
+    return std::make_unique<std::vector<Card *>>(dealtCards);
+}
