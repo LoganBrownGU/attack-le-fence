bd6fe4e1882eecc25f73fd4fabcfcfe3868e08a1
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-05-21 10:54:11 +0100

    Linked 2d library updated CMakeLists.txt to include simple2d Closes #17

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include "CardFactory.h"
 #include "player/LocalCLIPlayer.h"
 #include "Game.h"
+#include <simple2d.h>
+#include <random>
+
 
 int main() {
+
 	auto players = new std::vector<PlayerType>();
 	for (int i = 0; i < 3; ++i)
 		players->push_back(LOCAL_CLI);
